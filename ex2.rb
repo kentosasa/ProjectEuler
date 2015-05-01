a7d4e5af0326a5197acf93d11e655361759931b1
kento sasamoto <knote0721@gmail.com>
2015-05-01 22:03:37 +0900

    ex2

--- ex2.rb
+++ ex2.rb
@@ -4,17 +4,36 @@
 #
 #By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
-n1 = 2 #最新の値
-n2 = 1 #一個前の値
-sum = 3 #合計
+# コード01, 変数名・フィボナッチ関数の扱い方に改善の余地あり
 
-while n1 < 4000000 do
-	n3 = n1+n2
-	n2 = n1
-	n1 = n3
-	if n1%2 == 0
-		sum += n1
-	end
+#n1 = 2 #最新の値
+#n2 = 1 #一個前の値
+#sum = 3 #合計
+#
+#while n1 < 4000000 do
+#	n3 = n1+n2
+#	n2 = n1
+#	n1 = n3
+#	if n1%2 == 0
+#		sum += n1
+#	end
+#end
+#
+#puts sum
+
+
+# コード02, フィボナッチ数列を配列にする
+
+fibonacci_sequence = [0,1]
+sum = 0
+
+def fibonacci_sequence.add_next
+  self.push self[self.size-2] + self.last
+end
+
+while(fibonacci_sequence.last < 4000000) do
+  last = fibonacci_sequence.add_next().last
+  sum += last if last%2 == 0
 end
 
 puts sum
